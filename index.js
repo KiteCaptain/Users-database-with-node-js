7ba55685dd30ffcde865266521311986ab5de313
KiteCaptain <97241357+KiteCaptain@users.noreply.github.com>
2022-10-17 02:49:19 +0300

    tried searching the database

--- index.js
+++ index.js
@@ -21,30 +21,46 @@ const con = mysql.createConnection({
 //     }
 // )
 
-con.connect(function(err){
-    if (err) throw err;
-    console.log("connected!");
-    let sql = `INSERT INTO visitors(first_name, email, subject, message) VAlUES ?`
-    let values = [
-        ["John", "jo@gmail.com","Greetings", "Undecided"],
-        ["Joh", "joh@gmail.com","Greetings", "Undecided"],
-        ["faith", "faith@gmail.com","Greetings", "Undecided"],
-        ["Sanndy", "jSandy@gmail.com","Greetings", "Undecided"],
-        ["Betty", "Betty@gmail.com","Greetings", "Undecided"],
-        ["Vicky", "vicky@gmail.com","Greetings", "Undecided"],
-        ["chuck", "chuck@gmail.com","Greetings", "Undecided"],
-        ["ben", "ben@gmail.com","Greetings", "Undecided"],
-        ["william", "william@gmail.com","Greetings", "Undecided"],
-        ["Viola", "viola@gmail.com","Greetings", "Undecided"],
-        ["Amy", "amy@gmail.com","Greetings", "Undecided"]
+// con.connect(function(err){
+//     if (err) throw err;
+//     console.log("connected!");
+//     let sql = `INSERT INTO visitors(first_name, email, subject, message) VAlUES ?`
+//     let values = [
+//         ["John", "jo@gmail.com","Greetings", "Undecided"],
+//         ["Joh", "joh@gmail.com","Greetings", "Undecided"],
+//         ["faith", "faith@gmail.com","Greetings", "Undecided"],
+//         ["Sanndy", "jSandy@gmail.com","Greetings", "Undecided"],
+//         ["Betty", "Betty@gmail.com","Greetings", "Undecided"],
+//         ["Vicky", "vicky@gmail.com","Greetings", "Undecided"],
+//         ["chuck", "chuck@gmail.com","Greetings", "Undecided"],
+//         ["ben", "ben@gmail.com","Greetings", "Undecided"],
+//         ["william", "william@gmail.com","Greetings", "Undecided"],
+//         ["Viola", "viola@gmail.com","Greetings", "Undecided"],
+//         ["Amy", "amy@gmail.com","Greetings", "Undecided"]
 
-    ]
-    con.query(sql, [values], (err, result)=>{
-        if (err) console.log("Email already exists!"); 
-        console.log("Thank your, response helps us improve our services")
-        console.log(result)
+//     ]
+//     con.query(sql, [values], (err, result)=>{
+//         if (err) console.log("Email already exists!"); 
+//         console.log("Thank your, response helps us improve our services")
+//         console.log(result)
         
         
+//     })
+// })
+
+con.connect ((err) => {
+    if (err) throw err;
+    sql = `SELECT * FROM visitors`
+    con.query(sql, (err, result, fields)=>{
+        if (err) console.log("Try again! An error occured");
+        for (obj of result) {
+            console.log(obj)
+            for (let item in obj) {
+                if(item.email == "viola@gmail.com") {
+                    console.log("Dear " + item.first_name + ", your id is" + item.visitor_id)
+                }
+            }
+        }
     })
 })
 
