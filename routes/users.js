9b15c2e0f86ef013a03bcf6065b2c21bf7571fc7
KiteCaptain <97241357+KiteCaptain@users.noreply.github.com>
2022-10-19 01:35:46 +0300

    finished express

--- routes/users.js
+++ routes/users.js
@@ -2,30 +2,48 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res)=>{
-    res.send("All user list")
+    console.log(req.query.name)
+    res.send("User List")
 })
 router.get('/new', (req, res)=>{
-    res.send("Bring in a new user form")
+    res.render("users/new", {firstname: "Testing form"})
 })
+    
 
-router.post('/', (req, res)=>{
-    res.send('Create User')
+
+router.post("/", (req, res)=>{
+    const isValid = false
+    if(isValid) {
+        users.push({firstname : req.body.firstname})
+        res.redirect(`/users/${users.length-1}`)
+    } else {
+        console.log("ERROR!")
+        res.render('users/new', {firstname: req.body.firstname})
+    }
+    // console.log(req.body.firstname)
+    // res.send(`registration for ${req.body.firstname} was succesful!`)
 })
 
 router
     .route("/:id")
     .get((req, res)=>{  
+        console.log(req.user)
         res.send(`Get user with id ${req.params.id}`)
     })
     .put((req, res)=>{
-        res.send(`Update user with ID ${req.params.id}`)
+        console.log(`Put user with id ${req.params.id}`)
+        res.send(`Update user with ID ${req.params.id} `)
     })
     .delete((req, res)=>{
-        res.send(`Get user with ID ${req.params.id}`)
+        console.log(`Deleted user with id ${req.params.id}`)
+        res.send(`Delete user with ID ${req.params.id}`)
     })
 
+
+const users = [{name: "kite"}, {name: "Captain"}]
 router.param("id", (req, res, next, id)=>{
-    console.log(id)
+    req.user = users[id]
+    next()
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,20 +1,32 @@
 const express = require('express')
-
 const app = express()
-console.log("Running express app...")
+
+console.log("express app running...")
+
+
+app.use(express.static("public"))
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
 
 app.set('view engine', 'ejs')
-app.get('/',(req, res)=> {
-    console.log('Here')
-    // res.status(500).send("There was an error")
-    // res.status(500).json({message: "Internal server error"})
-    // res.json({message: "Success by default"})
-    // res.download("server.js")
-    res.render("index", { tet: "World" })
-})
+// app.get('/',(req, res)=> {
+//     console.log('Here')
+//     // res.status(500).send("There was an error")
+//     // res.status(500).json({message: "Internal server error"})
+//     // res.json({message: "Success by default"})
+//     // res.download("server.js")
+//     // res.render("index", { test: "Wold" })
+// })
 
+// app.use(logger)
+
+// function logger(req, res, next) {
+//     console.log(req.originalUrl)
+//     next()
+// }
 
 const userRouter = require('./routes/users')
+
 
 app.use("/users", userRouter)
 app.listen(3000)
