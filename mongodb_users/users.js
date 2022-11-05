d4cbd17d643ed8a0a6ceee8f29a702c223a6ec2f
KiteCaptain <97241357+KiteCaptain@users.noreply.github.com>
2022-11-06 01:01:21 +0300

    simple

--- mongodb_users/users.js
+++ mongodb_users/users.js
@@ -6,25 +6,15 @@ MongoClient.connect(uri, (err, db) => {
     if (err) throw err;
     let dbo = db.db("captaintechy");
     let myobj = [
-        { name: 'John', address: 'Highway 71'},
-        { name: 'Peter', address: 'Lowstreet 4'},
-        { name: 'Amy', address: 'Apple st 652'},
-        { name: 'Hannah', address: 'Mountain 21'},
-        { name: 'Michael', address: 'Valley 345'},
-        { name: 'Sandy', address: 'Ocean blvd 2'},
-        { name: 'Betty', address: 'Green Grass 1'},
-        { name: 'Richard', address: 'Sky st 331'},
-        { name: 'Susan', address: 'One way 98'},
-        { name: 'Vicky', address: 'Yellow Garden 2'},
-        { name: 'Ben', address: 'Park Lane 38'},
-        { name: 'William', address: 'Central st 954'},
-        { name: 'Chuck', address: 'Main Road 989'},
-        { name: 'Viola', address: 'Sideway 1633'}
+        { name: 'Chocolatte Heaveny'},
+        { name: 'Tastie Lemon'},
+        { name: 'Vamilla gream'}
       ];
 
     dbo.collection("customers").insertMany(myobj, (err, res) => {
         if (err) throw err;
         console.log(res.insertedCount + " collections inserted!" );
+        console.log(res)
 
         
         db.close()
